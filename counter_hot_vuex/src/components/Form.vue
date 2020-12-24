57314d7f4b2c88955742debac99c9ade36ac0b11
franck <fbeya@atecna.fr>
2020-12-24 01:03:13 +0100

    change state

--- counter_hot_vuex/src/components/Form.vue
+++ counter_hot_vuex/src/components/Form.vue
@@ -1,15 +1,15 @@
 <template>
   <v-container class="mx-auto text-center mt-5">
    <ul>
-     <li v-for="memb in member" :key="memb.id">
-       {{memb.name}}
-       <v-btn dark tile class="rounded" @click="removeMembers(memb.id)"> X </v-btn>
+     <li v-for="member in members" :key="member.id">
+       {{member.name}}
+       <v-btn dark tile class="rounded" @click="removeMembers(member.id)"> X </v-btn>
      </li>
    </ul>
     <v-card class="mx-auto mt-9" max-width="500" >
       <v-form>
       <v-card-text>
-          <v-text-field v-model="message" label="prenom" required outlined type="text"></v-text-field>
+          <v-text-field v-model="value" label="prenom" required outlined type="text"></v-text-field>
       </v-card-text>
       <v-card-actions>
         <v-col class="text-right">
@@ -26,25 +26,25 @@
 let index = 1
 export default{
   data: ()=>({
-  message: '',
-  member :[]
+  value: '',
+  members :[]
 }),
   methods: {
     addMembers (){
-      const nextMembers = this.message
+      const nextMembers = this.value
       if(nextMembers){
-        this.member.push({
+        this.members.push({
           id : index++,
           name: nextMembers
         })
       }
-      this.message =''
+      this.value =''
     },
     removeMembers(element){
-      this.member.splice(this.member.indexOf(element), 1);
-      /*this.member = this.member.filter(membe =>{
-        return membe.id !== element
-      })*/
+      // this.members.splice(this.members.indexOf(element), 1);
+      this.members = this.members.filter(member =>{
+        return member.id !== element
+      })
     }
   }
 }
